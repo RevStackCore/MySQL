4fcd447d1f7d1b989777da7e5d5771ddaa466e2a
Scott <dev@misonline.biz>
2019-01-18 01:42:06 -0500

    Nuked version 1

--- README.md
+++
@@ -1,2 +0,0 @@
-# MySQL
-MySQL implementation of the RevStack Repository Pattern.
--- RevStackCore.MySQL/Query/ProjectionRow.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace RevStackCore.MySQL.Query
-{
-    public abstract class ProjectionRow
-    {
-        public abstract object GetValue(int index);
-    }
-}
