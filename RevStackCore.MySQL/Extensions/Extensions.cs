974f5b96085ad020ba77f210b19bc76576209d30
Scott <dev@misonline.biz>
2019-02-28 12:13:41 -0500

    Added Poco First RegisterTable Extension method, updated version

---
+++ RevStackCore.MySQL/Extensions/Extensions.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using RevStackCore.Pattern;
+using RevStackCore.Extensions.SQL;
+
+
+namespace RevStackCore.MySQL
+{
+    public static class Extensions
+    {
+        public static MySQLDbContext RegisterTable<T, TKey>(this MySQLDbContext context) where T : class, IEntity<TKey>
+        {
+            string cmdText;
+            List<SqlDataColumn> indexes;
+            Type type = typeof(T);
+            var metadataSchema = type.ToMetadataSqlSchema();
+            metadataSchema = MySqlTypeMapping.Map(metadataSchema);
+            string conn = context.ConnectionString;
+            var utility = new SqlUtility<MySqlConnection, MySqlCommand>(conn);
+            if (utility.TableExists(metadataSchema.TableName))
+            {
+                var existingColumns = getTableColumns(conn, metadataSchema.TableName);
+                cmdText = alterTableCommandText(metadataSchema, existingColumns);
+                if (!string.IsNullOrEmpty(cmdText))
+                {
+                    utility.ExecuteSqlCommand(cmdText);
+                    indexes = getIndexes(metadataSchema, existingColumns);
+                    foreach (var index in indexes)
+                    {
+                        cmdText = createIndexCommandText(index, metadataSchema.TableName);
+                        utility.ExecuteSqlCommand(cmdText);
+                    }
+                }
+            }
+            else
+            {
+                cmdText = createTableCommandText(metadataSchema);
+                utility.ExecuteSqlCommand(cmdText);
+                indexes = getIndexes(metadataSchema);
+                foreach (var index in indexes)
+                {
+                    cmdText = createIndexCommandText(index, metadataSchema.TableName);
+                    utility.ExecuteSqlCommand(cmdText);
+                }
+            }
+
+            return context;
+        }
+
+        private static List<string> getTableColumns(string connectionString, string tableName)
+        {
+            var columns = new List<string>();
+            string query = $"SELECT * FROM {tableName}";
+            DataTable schema = null;
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                using (var schemaCommand = new MySqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (var reader = schemaCommand.ExecuteReader(CommandBehavior.SchemaOnly))
+                    {
+                        schema = reader.GetSchemaTable();
+                    }
+                }
+            }
+            foreach (DataRow col in schema.Rows)
+            {
+                columns.Add(col.ItemArray[0].ToString());
+            }
+
+            return columns;
+        }
+
+        private static string createTableCommandText(MetadataSqlSchema schema)
+        {
+            string cmd = $"CREATE TABLE {schema.TableName}( ";
+            var columns = schema.Columns;
+            foreach (var column in columns)
+            {
+                cmd += $"{column.Name} {column.DbTypeName}";
+                if (!column.AllowNulls)
+                {
+                    cmd += $" NOT NULL";
+                }
+                if (column.IsAutoIncrementing)
+                {
+                    cmd += $" AUTO_INCREMENT";
+                }
+                cmd += ",";
+            }
+            string primaryKey = getPrimaryKeyText(schema);
+            cmd += $" {primaryKey})";
+            return cmd;
+        }
+
+        private static string alterTableCommandText(MetadataSqlSchema schema, List<string> existingColumns)
+        {
+            string cmd = null;
+            var schemaColumns = schema.Columns;
+            var columns = schemaColumns.Where(x => !existingColumns.Any(y => y == x.Name));
+            if (columns.Any())
+            {
+                cmd = $"ALTER TABLE {schema.TableName} ADD ";
+                int count = columns.Count();
+                int index = 0;
+                foreach (var column in columns)
+                {
+                    index += 1;
+                    cmd += $"{column.Name} {column.DbTypeName}";
+                    if (!column.AllowNulls)
+                    {
+                        cmd += $" NOT NULL";
+                    }
+                    if (column.IsAutoIncrementing)
+                    {
+                        cmd += $" AUTO_INCREMENT";
+                    }
+                    if (index < count)
+                    {
+                        cmd += ", ";
+                    }
+                }
+            }
+            return cmd;
+        }
+
+        private static string createIndexCommandText(SqlDataColumn column, string tableName)
+        {
+            string cmd = "CREATE";
+            if (column.IsUniqueIndex)
+            {
+                cmd += " UNIQUE";
+            }
+            if (column.IsClusteredIndex)
+            {
+                cmd += " CLUSTERED";
+            }
+            cmd += $" INDEX idx_{column.Name} ON {tableName}({column.Name})";
+            return cmd;
+        }
+
+        private static List<SqlDataColumn> getIndexes(MetadataSqlSchema schema)
+        {
+            var indexes = schema.Columns.Where(x => x.IsIndex == true);
+            if (indexes.Any())
+            {
+                return indexes.ToList();
+            }
+            else
+            {
+                return new List<SqlDataColumn>();
+            }
+        }
+
+        private static List<SqlDataColumn> getIndexes(MetadataSqlSchema schema, List<string> existingColumns)
+        {
+            var schemaColumns = schema.Columns;
+            var columns = schemaColumns.Where(x => !existingColumns.Any(y => y == x.Name));
+            if (columns.Any())
+            {
+                return columns.ToList();
+            }
+            else
+            {
+                return new List<SqlDataColumn>();
+            }
+        }
+
+        private static string getPrimaryKeyText(MetadataSqlSchema schema)
+        {
+            string ids = "Id";
+            string cmd = $"CONSTRAINT PK_{schema.TableName} PRIMARY KEY";
+            var columns = schema.Columns.Where(x => x.IsPrimaryKey == true);
+            if (columns.Any())
+            {
+                int count = columns.Count();
+                int index = 0;
+                ids = "";
+                foreach (var column in columns)
+                {
+                    index += 1;
+                    ids += column.Name;
+                    if (index < count)
+                    {
+                        ids += ",";
+                    }
+                }
+            }
+            cmd += $"({ids})";
+            return cmd;
+        }
+    }
+}
---
+++ RevStackCore.MySQL/Mappings/MySqlTypeMapping.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Data;
+using RevStackCore.Extensions.SQL;
+
+namespace RevStackCore.MySQL
+{
+    public static class MySqlTypeMapping
+    {
+        public static MetadataSqlSchema Map(MetadataSqlSchema schema)
+        {
+            foreach (var column in schema.Columns)
+            {
+                column.DbTypeName = column.ToDbTypeName();
+            }
+
+            return schema;
+        }
+
+        private static string ToDbTypeName(this SqlDataColumn column)
+        {
+            if (column.DbType != default(SqlDbType))
+            {
+                return column.ToDbTypeNameFromSqlType();
+            }
+            else
+            {
+                return column.Type.ToDbTypeNameFromType();
+            }
+        }
+
+        private static string ToDbTypeNameFromSqlType(this SqlDataColumn column)
+        {
+            var type = column.DbType;
+            var size = column.Size;
+            var precision = column.Precision;
+            if (type == SqlDbType.BigInt)
+            {
+                return "bigint";
+            }
+            else if (type == SqlDbType.Bit)
+            {
+                return "tinyint(1)";
+            }
+            else if (type == SqlDbType.Binary)
+            {
+                return "binary";
+            }
+            else if (type == SqlDbType.Char)
+            {
+                if (size != null)
+                {
+                    return $"char({size})";
+                }
+                return "char";
+            }
+            else if (type == SqlDbType.Date)
+            {
+                return "date";
+            }
+            else if (type == SqlDbType.DateTime)
+            {
+                return "datetime";
+            }
+            else if (type == SqlDbType.DateTime2)
+            {
+                return "datetime";
+            }
+            else if (type == SqlDbType.DateTimeOffset)
+            {
+                return "datetime";
+            }
+            else if (type == SqlDbType.Decimal)
+            {
+                if (size != null && precision != null)
+                {
+                    return $"decimal({size},{precision})";
+                }
+                else if (size != null)
+                {
+                    return $"decimal({size})";
+                }
+                else
+                {
+                    return "decimal";
+                }
+            }
+            else if (type == SqlDbType.Float)
+            {
+                if (size != null && precision != null)
+                {
+                    return $"float({size},{precision})";
+                }
+                else if (size != null)
+                {
+                    return $"float({size})";
+                }
+                else
+                {
+                    return "float";
+                }
+            }
+            else if (type == SqlDbType.Image)
+            {
+                return "binary";
+            }
+            else if (type == SqlDbType.Int)
+            {
+                return "int";
+            }
+            else if (type == SqlDbType.BigInt)
+            {
+                if (size != null)
+                {
+                    return $"bigint({size})";
+                }
+                else
+                {
+                    return "bigint";
+                }
+            }
+            else if (type == SqlDbType.Money)
+            {
+                return "decimal(12,2)";
+            }
+            else if (type == SqlDbType.NChar)
+            {
+                if (size != null)
+                {
+                    return $"char({size})";
+                }
+                else
+                {
+                    return "char";
+                }
+            }
+            else if (type == SqlDbType.NVarChar)
+            {
+                if (size != null)
+                {
+                    return $"varchar({size})";
+                }
+                else
+                {
+                    return "varchar(100)";
+                }
+            }
+            else if (type == SqlDbType.Real)
+            {
+                if (size != null && precision != null)
+                {
+                    return $"real({size},{precision})";
+                }
+                else if (size != null)
+                {
+                    return $"real({size})";
+                }
+                else
+                {
+                    return "real";
+                }
+            }
+            else if (type == SqlDbType.SmallInt)
+            {
+                return "smallint";
+            }
+            else if (type == SqlDbType.SmallMoney)
+            {
+                return "decimal(5,2)";
+            }
+            else if (type == SqlDbType.SmallDateTime)
+            {
+                return "datetime";
+            }
+            else if (type == SqlDbType.Text)
+            {
+                return "longtext";
+            }
+            else if (type == SqlDbType.Time)
+            {
+                return "time";
+            }
+            else if (type == SqlDbType.TinyInt)
+            {
+                return "tinyint";
+            }
+            else if (type == SqlDbType.UniqueIdentifier)
+            {
+                return "timestamp";
+            }
+            else if (type == SqlDbType.UniqueIdentifier)
+            {
+                return "varchar(64)";
+            }
+            else if (type == SqlDbType.VarBinary)
+            {
+                return "varbinary";
+            }
+            else if (type == SqlDbType.VarChar)
+            {
+                if (size != null)
+                {
+                    return $"varchar({size})";
+                }
+                else
+                {
+                    return "varchar(100)";
+                }
+            }
+
+            return type.ToString();
+        }
+
+        private static string ToDbTypeNameFromType(this Type type)
+        {
+            if (type == typeof(string))
+            {
+                return "varchar(100)";
+            }
+            else if (type == typeof(int))
+            {
+                return "int";
+            }
+            else if (type == typeof(short))
+            {
+                return "int";
+            }
+            else if (type == typeof(long))
+            {
+                return "bigint";
+            }
+            else if (type == typeof(decimal))
+            {
+                return "decimal";
+            }
+            else if (type == typeof(float))
+            {
+                return "float";
+            }
+            else if (type == typeof(double))
+            {
+                return "float";
+            }
+            else if (type == typeof(DateTime))
+            {
+                return "datetime";
+            }
+            else if (type == typeof(bool))
+            {
+                return "tinyint(1)";
+            }
+            else if (type == typeof(DateTimeOffset))
+            {
+                return "datetime";
+            }
+            else if (type == typeof(Byte[]))
+            {
+                return "binary";
+            }
+            else if (type == typeof(TimeSpan))
+            {
+                return "time";
+            }
+            else if (type == typeof(char[]))
+            {
+                return "char";
+            }
+            else if (type == typeof(Guid))
+            {
+                return "varchar(64)";
+            }
+
+            return "varchar(100)";
+
+        }
+    }
+}
